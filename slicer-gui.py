a096b5322e75fd9d5f2e3616dce32ed1a1cdde54
笛鹿FlutyDeer <68263367+flutydeer@users.noreply.github.com>
2023-01-29 14:55:49 +0800

    Write console outputs to log file.

--- slicer-gui.py
+++ slicer-gui.py
@@ -1,4 +1,6 @@
+import os
 import sys
+import datetime
 
 from PySide6 import QtCore
 from PySide6.QtWidgets import QApplication
@@ -7,6 +9,13 @@ from PySide6.QtGui import QFont
 import gui.mainwindow
 
 if __name__ == '__main__':
+    # Write console outputs to log file.
+    __stderr__ = sys.stderr
+    date_time = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+    folder = os.path.exists('log')
+    if not folder: 
+        os.makedirs('log')
+    sys.stderr = open(f'log/log {date_time}.txt', 'w')
     
     app = QApplication(sys.argv)
     app.setApplicationName("Audio Slicer")
