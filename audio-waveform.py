dfd55a62f119d58de2402c541c8de2bd6d7270b9
笛鹿FlutyDeer <68263367+flutydeer@users.noreply.github.com>
2023-05-20 01:40:38 +0800

    Add light theme on waveform

--- audio-waveform.py
+++ audio-waveform.py
@@ -1,7 +1,28 @@
 import soundfile
-import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+dark_theme_palette = {
+    'primary': '#8ab4f7',
+    'accent': 'orange',
+    'page_background': '#202124',
+    'figure_frame': '#404040',
+    'figure_background': '#202020',
+    'axis': '#A0A0A0',
+    'title': '#e4e7eb',
+    'grid': '#404040'
+}
+light_theme_palette = {
+    'primary': '#1a73e8',
+    'accent': 'orange',
+    'page_background': '#f8f9fa',
+    'figure_frame': '#B0B0B0',
+    'figure_background': '#F0F0F0',
+    'axis': '#505050',
+    'title': '#4d5157',
+    'grid': '#B0B0B0'
+}
 
 
 def showWaveForm(filename):
@@ -14,59 +35,60 @@ def showWaveForm(filename):
     # to mono
     time = np.arange(0, len(audio_samples)) * (1.0 / sr)
 
+    palette = light_theme_palette
     plt.rcParams['toolbar'] = 'None'
-    plt.rcParams['axes.facecolor'] = '#202020'
+    plt.rcParams['axes.facecolor'] = palette['figure_background']
     plt.figure(figsize=(10, 6))
-    plt.plot(time, audio_samples, color='#8ab4f7')
+    plt.plot(time, audio_samples, color=palette['primary'])
     fig = plt.gcf()
-    fig.set_facecolor("#202124")
+    fig.set_facecolor(palette['page_background'])
     ax = plt.gca()
-    ax.spines['top'].set_color('#404040')
-    ax.spines['bottom'].set_color('#404040')
-    ax.spines['left'].set_color('#404040')
-    ax.spines['right'].set_color('#404040')
+    ax.spines['top'].set_color(palette['figure_frame'])
+    ax.spines['bottom'].set_color(palette['figure_frame'])
+    ax.spines['left'].set_color(palette['figure_frame'])
+    ax.spines['right'].set_color(palette['figure_frame'])
     ax.tick_params(
         axis='x',
-        color='#A0A0A0',
-        labelcolor='#A0A0A0'
+        color=palette['axis'],
+        labelcolor=palette['axis']
     )
     ax.tick_params(
         axis='y',
-        color='#A0A0A0',
-        labelcolor='#A0A0A0',
+        color=palette['axis'],
+        labelcolor=palette['axis']
     )
     plt.title(
         label='Audio WaveForm',
         fontdict={
-            "color": "#e4e7eb"
+            "color": palette['title']
         }
     )
     plt.xlabel(
         xlabel="Time/s",
         fontdict={
-            "color": "#A0A0A0"
+            "color": palette['axis']
         }
     )
     # plt.ylabel(
     #     ylabel="Value",
     #     fontdict={
-    #         "color": "#A0A0A0"
+    #         "color": palette['axis']
     #     }
     # )
+    plt.grid(
+        color=palette['grid']
+    )
     plt.autoscale(enable=True, axis='x', tight=True)
     plt.ylim((-1, 1))
-    plt.grid(
-        color='#404040'
-    )
     plt.annotate("Slice #1",
                  xy=(53, 1),
                  xycoords=("data", "axes fraction"),
-                 color='orange'
+                 color=palette['accent']
                  )
-    plt.axvline(x=53, color='orange')
+    plt.axvline(x=53, color=palette['accent'])
     plt.show()
 
 
 if __name__ == "__main__":
     # showWaveForm("D:/测试/slicer测试音频/2009000334.wav")
-    showWaveForm("D:/测试/slicer测试音频/Vocal (5).wav")
+    showWaveForm("D:\\编曲学习\\眉间雪\\眉间雪-1-洛天依.wav")
